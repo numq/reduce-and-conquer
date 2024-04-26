1567929bf7a5233eae5560c6c7c17d95c12de6fe
numq <numq.dev@gmail.com>
2024-04-27 01:36:24 +0300

    Provide refactoring

--- composeApp/src/commonMain/kotlin/feature/Feature.kt
+++ composeApp/src/commonMain/kotlin/feature/Feature.kt
@@ -1,29 +1,28 @@
 package feature
 
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.*
 
 abstract class Feature<State, Message, Effect : feature.Effect<*>>(
     initialState: State,
     coroutineScope: CoroutineScope,
 ) {
-    private val messages = Channel<Message>(Channel.UNLIMITED)
+    private val messages = MutableSharedFlow<Message>(extraBufferCapacity = Int.MAX_VALUE)
 
     private val _state = MutableStateFlow(initialState)
     val state = _state.asStateFlow()
 
-    private val _effects = Channel<Effect>(Channel.UNLIMITED)
-    val effects = _effects.receiveAsFlow().shareIn(scope = coroutineScope, started = SharingStarted.Lazily)
+    private val _effects = MutableSharedFlow<Effect>(extraBufferCapacity = Int.MAX_VALUE)
+    val effects = _effects.asSharedFlow()
 
-    fun dispatchMessage(message: Message) = messages.trySend(message).isSuccess
+    fun dispatchMessage(message: Message) = messages.tryEmit(message)
 
-    fun performEffect(effect: Effect) = _effects.trySend(effect).isSuccess
+    fun performEffect(effect: Effect) = _effects.tryEmit(effect)
 
     abstract suspend fun reduce(state: State, message: Message): State
 
     init {
-        messages.receiveAsFlow().onEach { message ->
+        messages.onEach { message ->
             _state.value = reduce(_state.value, message)
         }.launchIn(coroutineScope)
     }
