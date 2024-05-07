e09215dd625ff07e7fb0e00e91f38a6961c734cd
numq <numq.dev@gmail.com>
2024-05-08 01:26:11 +0300

    Provide refactoring

--- composeApp/src/commonMain/kotlin/feature/Feature.kt
+++ composeApp/src/commonMain/kotlin/feature/Feature.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-abstract class Feature<in Command, out State, Event>(
+abstract class Feature<in Command, out State, out Event>(
     initialState: State,
     private val reducer: Reducer<Command, State, Event>,
 ) {
@@ -14,10 +14,6 @@ abstract class Feature<in Command, out State, Event>(
 
     private val _events = MutableSharedFlow<Event>()
     val events = _events.asSharedFlow()
-
-    suspend fun trigger(event: Event) = runCatching {
-        _events.emit(event)
-    }.isSuccess
 
     suspend fun execute(command: Command) = runCatching {
         reducer.reduce(state.value, command).let { (state, event) ->
