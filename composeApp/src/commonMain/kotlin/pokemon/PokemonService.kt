464f8df6b0453fd03c89e0c4fbe8aabf99e0c9d8
numq <numq.dev@gmail.com>
2024-05-17 01:21:54 +0300

    Provide refactoring

--- composeApp/src/commonMain/kotlin/pokemon/PokemonService.kt
+++ composeApp/src/commonMain/kotlin/pokemon/PokemonService.kt
@@ -12,17 +12,17 @@ interface PokemonService {
         private val jsonParser: Json,
     ) : PokemonService {
         companion object {
-            const val pokedexJsonPath = "files/pokedex.json"
-            const val pokemonImagePath = "drawable/pokemon/%s.png"
+            const val POKEDEX_JSON_PATH = "files/pokedex.json"
+            const val POKEMON_IMAGE_PATH = "drawable/pokemon/%s.png"
         }
 
-        override suspend fun getPokemons() = fileProvider.open(pokedexJsonPath)
+        override suspend fun getPokemons() = fileProvider.open(POKEDEX_JSON_PATH)
             .mapCatching(ByteArray::decodeToString)
             .mapCatching<Array<PokemonJson>, String>(jsonParser::decodeFromString)
             .mapCatching(Array<PokemonJson>::toList)
 
         override suspend fun getPokemonImageById(id: Int) = "$id".padStart(3, '0').let { imageName ->
-            fileProvider.open(pokemonImagePath.replace("%s", imageName))
+            fileProvider.open(POKEMON_IMAGE_PATH.replace("%s", imageName))
         }
     }
 }
