200ed8b8512e3709d50c0ec5292d738d29596595
numq <numq.dev@gmail.com>
2024-05-19 04:03:41 +0300

    Provide feature test changes

--- composeApp/src/commonTest/kotlin/feature/FeatureTest.kt
+++ composeApp/src/commonTest/kotlin/feature/FeatureTest.kt
@@ -1,33 +1,31 @@
 package feature
 
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.test.StandardTestDispatcher
-import kotlinx.coroutines.test.resetMain
-import kotlinx.coroutines.test.runTest
-import kotlinx.coroutines.test.setMain
+import kotlinx.coroutines.test.*
 import kotlin.test.*
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class FeatureTest {
     private val testDispatcher = StandardTestDispatcher()
 
-    private lateinit var feature: Feature<TestCommand, TestState, TestEvent>
+    private val testScope = TestScope(testDispatcher)
 
     private val events = mutableListOf<TestEvent>()
+
+    private lateinit var feature: Feature<TestCommand, TestState, TestEvent>
 
     @BeforeTest
     fun setUp() {
         Dispatchers.setMain(testDispatcher)
 
-        feature = object : Feature<TestCommand, TestState, TestEvent>(TestState(0), TestReducer()) {
+        feature = object : Feature<TestCommand, TestState, TestEvent>(TestState(0), TestReducer()) {}
 
-        }
-
-        feature.events.onEach(events::add).launchIn(CoroutineScope(testDispatcher))
+        feature.events.onEach { event ->
+            events.add(event)
+        }.launchIn(testScope)
     }
 
     @AfterTest
@@ -41,21 +39,13 @@ class FeatureTest {
     fun updateStateAndEmitEvent() = runTest {
         assertEquals(0, feature.state.value.count)
 
-        feature.execute(TestCommand.Increment)
-
+        assertTrue(feature.execute(TestCommand.Increment))
         assertEquals(1, feature.state.value.count)
 
-        assertEquals(TestEvent.Incremented, events.last())
-
-        feature.execute(TestCommand.Decrement)
-
+        assertTrue(feature.execute(TestCommand.Decrement))
         assertEquals(0, feature.state.value.count)
 
-        assertEquals(TestEvent.Decremented, events.last())
-    }
-
-    @Test
-    fun returnTrueOnExecutionSuccess() = runTest {
-        assertTrue(feature.execute(TestCommand.Increment))
+        testDispatcher.scheduler.advanceUntilIdle()
+        assertEquals(listOf(TestEvent.Incremented, TestEvent.Decremented), events)
     }
 }
