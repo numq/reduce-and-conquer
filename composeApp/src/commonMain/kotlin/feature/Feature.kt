5ffb4ab9b31d6773194b985d98e00d2659537a44
numq <numq.dev@gmail.com>
2024-05-26 02:37:34 +0300

    Provide refactoring

--- composeApp/src/commonMain/kotlin/feature/Feature.kt
+++ composeApp/src/commonMain/kotlin/feature/Feature.kt
@@ -2,13 +2,14 @@ package feature
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.receiveAsFlow
 
 abstract class Feature<in Command, out State, out Event>(
     initialState: State,
+    eventBufferCapacity: Int = 64,
     private val reducer: Reducer<Command, State, Event>,
 ) {
     val coroutineScope = CoroutineScope(Dispatchers.Default)
@@ -16,13 +17,13 @@ abstract class Feature<in Command, out State, out Event>(
     private val _state = MutableStateFlow(initialState)
     val state = _state.asStateFlow()
 
-    private val _events = Channel<Event>(Channel.BUFFERED)
-    val events = _events.receiveAsFlow()
+    private val _events = MutableSharedFlow<Event>(extraBufferCapacity = eventBufferCapacity)
+    val events = _events.asSharedFlow()
 
     suspend fun execute(command: Command) = runCatching {
         reducer.reduce(state.value, command).let { (state, events) ->
             _state.emit(state)
-            events.forEach { event -> _events.send(event) }
+            events.forEach { event -> _events.emit(event) }
         }
     }.isSuccess
 }
