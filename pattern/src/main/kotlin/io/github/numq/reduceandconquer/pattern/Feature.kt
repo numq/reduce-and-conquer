666c0601cb15649e4d9de0c9810203ed894c32c2
numq <numq.dev@gmail.com>
2026-01-23 14:12:09 +0300

    Provide refactoring

--- pattern/src/main/kotlin/io/github/numq/reduceandconquer/pattern/Feature.kt
+++ pattern/src/main/kotlin/io/github/numq/reduceandconquer/pattern/Feature.kt
@@ -1,12 +1,25 @@
 package io.github.numq.reduceandconquer.pattern
 
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
 
+@OptIn(ExperimentalStdlibApi::class)
 interface Feature<out State, in Command, out Event> : AutoCloseable {
     val state: StateFlow<State>
 
     val events: Flow<Event>
 
     suspend fun execute(command: Command)
+
+    companion object {
+        operator fun <State, Command, Event> invoke(
+            initialState: State,
+            scope: CoroutineScope,
+            reducer: Reducer<State, Command, Event>,
+            vararg initialCommands: Command
+        ): Feature<State, Command, Event> = ReducerFeature(
+            initialState = initialState, initialCommands = initialCommands.toList(), scope = scope, reducer = reducer
+        )
+    }
 }
